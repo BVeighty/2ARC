8027a214f6e65851b2303ef5915199b57e00c641
Alban Durrheimer <alban6800@hotmail.fr>
2018-06-07 01:36:28 -0700

    Add comment

--- main.cpp
+++ main.cpp
@@ -12,14 +12,14 @@
 using namespace std;
 
 
-void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet);
+void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_char* packet); // Fonction permettant de lire le contenu de chaque paquet
 
 int main() {
   pcap_t *descr;
   char errbuf[PCAP_ERRBUF_SIZE];
 
   int mode;
-  cout << "Do you want to open a local capture file and test the rules theoretically? (0) \nOr do you want to use the program in real time? (1) \n -> Your choice? ";
+  cout << "Do you want to test the rules logic from a local capture file? (0) \nOr since then on a real-time capture? (1) \n -> Your choice? ";
   cin >> mode;
 
     if(mode) {
@@ -32,7 +32,7 @@ int main() {
             return 1;
         }
 
-        descr = pcap_open_live(dev, BUFSIZ, 0, -1, errbuf);
+        descr = pcap_open_live(dev, BUFSIZ, 0, -1, errbuf); // Lie en temps réel les paquets réseau
 
         if (descr == NULL) {
             cout << "pcap_open_live() failed: " << errbuf << endl;
@@ -44,7 +44,7 @@ int main() {
         cout << "File Location ? ";
         cin >> fileLocation;
 
-        descr = pcap_open_offline("/home/administrator/Downloads/test.pcapng", errbuf);
+        descr = pcap_open_offline("/home/administrator/Downloads/test.pcapng", errbuf); // Ouvre un fichier pcap
 
         if (descr == NULL) {
             cout << "pcap_open_live() failed: " << errbuf << endl;
@@ -52,11 +52,11 @@ int main() {
         }
     }
 
-    pcap_loop(descr, 0, packetHandler, NULL);
+    pcap_loop(descr, 0, packetHandler, NULL); // Lie entièrement un fichier pcap ou lie à l’infini les paquets réseau en exécutant pour chaque paquet la fonction « packetHandler »
     return 0;
 }
 
-string intConverter(int a)
+string intConverter(int a) // Convertis un type int en string
 {
     string result;
     ostringstream s;
@@ -66,7 +66,7 @@ string intConverter(int a)
     return result;
 }
 
-string charConverter(char a[])
+string charConverter(char a[]) // Convertis un type char en string
 {
     string result;
     ostringstream s;
@@ -80,11 +80,11 @@ string readRule(int a)
 {
 	if(a == 0)
 	{
-		return "Granted";
+		return "Accept";
 	}
 	if(a == 1)
 	{
-		return "Denied";
+		return "Drop";
 	}
 }
 
@@ -94,24 +94,24 @@ void packetHandler(u_char *userData, const struct pcap_pkthdr* pkthdr, const u_c
   const struct tcphdr* tcpHeader;
   char ipAddress[INET_ADDRSTRLEN];
   string sourceIp, destIp;
-  int port;
   string sourcePort, destPort;
+  int port;
   ip_regles ipRules;
   port_regles portRules;
 
   ethernetHeader = (struct ether_header*)packet;
   if (ntohs(ethernetHeader->ether_type) == ETHERTYPE_IP) {
-      ipHeader = (struct ip*)(packet + sizeof(struct ether_header));
-      inet_ntop(AF_INET, &(ipHeader->ip_src), ipAddress, INET_ADDRSTRLEN);
+      ipHeader = (struct ip*)(packet + sizeof(struct ether_header)); // Récupère les informations en rapport avec les ips du paquet
+      inet_ntop(AF_INET, &(ipHeader->ip_src), ipAddress, INET_ADDRSTRLEN); // Récupère l'ip source
       sourceIp = charConverter(ipAddress);
-      inet_ntop(AF_INET, &(ipHeader->ip_dst), ipAddress, INET_ADDRSTRLEN);
+      inet_ntop(AF_INET, &(ipHeader->ip_dst), ipAddress, INET_ADDRSTRLEN); // Récupère l'ip de destination
       destIp = charConverter(ipAddress);
 
       if (ipHeader->ip_p == IPPROTO_TCP) {
-          tcpHeader = (tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip));
-          port = ntohs(tcpHeader->source);
+          tcpHeader = (tcphdr*)(packet + sizeof(struct ether_header) + sizeof(struct ip)); // Récupère les informations en rapport avec les ports du paquet
+          port = ntohs(tcpHeader->source); // Récupère le port de la source du paquet
           sourcePort = intConverter(port);
-          port = ntohs(tcpHeader->dest);
+          port = ntohs(tcpHeader->dest); // Récupère le port de la destination du paquet
           destPort = intConverter(port);
 
           cout << sourceIp << ":" << sourcePort << " -> " << destIp << ":" << destPort << endl;
